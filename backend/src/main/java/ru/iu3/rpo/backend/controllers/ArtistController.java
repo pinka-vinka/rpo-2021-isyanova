92d1d845fb1713836e7981d48b61d3a7ae5e5cf0
pinka-vinka <iam2701@yandex.ru>
2021-06-15 10:52:45 +0300

    L8-L11

--- backend/src/main/java/ru/iu3/rpo/backend/controllers/ArtistController.java
+++ backend/src/main/java/ru/iu3/rpo/backend/controllers/ArtistController.java
@@ -1,6 +1,9 @@
 package ru.iu3.rpo.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import javax.validation.Valid;
@@ -10,21 +13,34 @@ import ru.iu3.rpo.backend.models.Artist;
 import ru.iu3.rpo.backend.models.Country;
 import ru.iu3.rpo.backend.repositories.ArtistRepository;
 import ru.iu3.rpo.backend.repositories.CountryRepository;
+import ru.iu3.rpo.backend.tools.DataValidationException;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class ArtistController {
     @Autowired
     ArtistRepository artistRepository;
+    @Autowired
     CountryRepository countryRepository;
 
     @GetMapping("/artists")
-    public List<Artist> getAllArtists() {
-        return artistRepository.findAll();
+    public Page<Artist> getAllArtists(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+        return artistRepository.findAll(PageRequest.of(page, limit, Sort.by(Sort.Direction.ASC,"name")));
+    }
+
+    @GetMapping("/artists/{id}")
+    public ResponseEntity<Artist> getArtist(@PathVariable(value = "id") Long artistId)
+        throws DataValidationException
+    {
+        Artist artist = artistRepository.findById(artistId)
+                .orElseThrow(()-> new DataValidationException("Художник с таким индексом не найден"));
+        return ResponseEntity.ok(artist);
     }
 
     @PostMapping("/artists")
@@ -58,19 +74,9 @@ public class ArtistController {
         return ResponseEntity.ok(artist);
     }
 
-    @DeleteMapping("/artists/{id}")
-    public Map<String, Boolean> deleteArtist(@PathVariable(value = "id") Long artistId) {
-        Optional<Artist> artist = artistRepository.findById(artistId);
-        Map<String, Boolean> response = new HashMap<>();
-        if (artist.isPresent())
-        {
-            artistRepository.delete(artist.get());
-            response.put("deleted", Boolean.TRUE);
-        }
-        else
-        {
-            response.put("deleted", Boolean.FALSE);
-        }
-        return response;
+    @PostMapping("/deleteartists")
+    public ResponseEntity deleteArtists(@Valid @RequestBody List<Artist> artists){
+        artistRepository.deleteAll(artists);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
